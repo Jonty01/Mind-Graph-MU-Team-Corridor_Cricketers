7a698bedff9a82d93eb0f6d60ae5c2869b4bc74a
hkm-007 <69042306+hkm-007@users.noreply.github.com>
2022-11-21 11:23:13 +0530

    Update plots.py

--- plots.py
+++ plots.py
@@ -5,18 +5,7 @@ import altair as alt
 import matplotlib.pyplot as plt
 
 st.title('Student Profile Analysis')
-# energy_source = pd.DataFrame({
-#     "EnergyType": ["Electricity","Gasoline","Natural Gas","Electricity","Gasoline","Natural Gas","Electricity","Gasoline","Natural Gas"],
-#     "Price ($)":  [150,73,15,130,80,20,170,83,20],
-#     "Date": ["2022-1-23", "2022-1-30","2022-1-5","2022-2-21", "2022-2-1","2022-2-1","2022-3-1","2022-3-1","2022-3-1"]
-#     })
 
-# option = st.selectbox(
-#      'Select type',
-#      ('Clubs participants data', 'Fest participants data','Clubs organisers data','Fest organisers data','No of participants in fests 1 and fest 2 from club 1','No of participants in fests 1 and fest 2 from club 2','No of participants in fests 1 and fest 2 from club 3','No of students participating in various events in fest 1','No of students participating in various events in fest 2','No of students participating in various events in various clubs'))
-# # keep the option and select boxes on the left side of the screen
-
-# make an option box at the left side of the screen
 option = st.sidebar.selectbox(
         'Select type',
         ('Clubs participants and organisers data', 'Fest participants and organisers data','No of participants in fests 1 and fest 2 from club 1','No of participants in fests 1 and fest 2 from club 2','No of participants in fests 1 and fest 2 from club 3','No of students participating in various events in fest 1','No of students participating in various events in fest 2','No of students participating in various events in various clubs'))
@@ -74,48 +63,6 @@ elif option == 'Fest participants and organisers data':
     )
  
     st.altair_chart(bar_chart, use_container_width=True)
-
-    # code = '''  "Energy Costs By Month"
-    # source = pd.DataFrame({
-    #     'Price ($)': [10, 15, 20],
-    #     'Month': ['January', 'February', 'March']
-    # })
- 
-    # bar_chart = alt.Chart(source).mark_bar().encode(
-    #     y='Price ($)',
-    #     x='Month',
-    # )
-    # st.altair_chart(bar_chart, use_container_width=True)
-    #  '''
-    # st.code(code, language='python')
-# elif option == 'Clubs organisers data':
-#     "Organisers data for clubs"
-#     source = pd.DataFrame({
-#         'Organisers': [15,15,15],
-#         'Total': ['Club1','Club2','Club3'],
-#      })
- 
-#     bar_chart = alt.Chart(source).mark_bar().encode(
-#         y='Total',
-#         x='Organisers',
-#     )
- 
-#     st.altair_chart(bar_chart, use_container_width=True)
-
-
-# elif option == 'Fest organisers data':
-#     "Organisers data for clubs"
-#     source = pd.DataFrame({
-#         'Organisers': [72,83],
-#         'Total': ['Fest1','Fest2'],
-#      })
- 
-#     bar_chart = alt.Chart(source).mark_bar().encode(
-#         y='Total',
-#         x='Organisers',
-#     )
- 
-#     st.altair_chart(bar_chart, use_container_width=True)
 
     
 elif option == 'No of participants in fests 1 and fest 2 from club 1':
@@ -182,10 +129,6 @@ elif option == 'No of students participating in various events in fest 2':
 
 elif option == 'No of students participating in various events in various clubs':
     "participants in club 1 events"
-    # source = pd.DataFrame({
-    #     'No of students': [134, 136, 67],
-    #     'Events': ['Event1','Event2','Event3'],
-    #  })
     source = pd.DataFrame({
         'No of students': [134, 136, 67],
         'Events': ['Event1','Event2','Event3'],
@@ -230,11 +173,3 @@ elif option == 'No of students participating in various events in various clubs'
     )
     
     st.altair_chart(bar_chart, use_container_width=True)
-    
- 
-    # bar_chart = alt.Chart(source).mark_bar().encode(
-    #     y='Events',
-    #     x='No of students',
-    # )
- 
-    # st.altair_chart(bar_chart, use_container_width=True)
